9fb86bc14de76692e7266c7bccee0a77a8a77c15
Giulia Tognon <giuliatognon@Mac.homenet.telecomitalia.it>
2025-10-16 13:54:57 +0200

    Adding Docker

--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -6,15 +6,20 @@ using Pokedex;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5094");
+
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<IPokemonService, PokemonService>();
 builder.Services.AddHttpClient<ITranslationService, TranslationService>();
 builder.Services.AddScoped<ITranslationStrategy, YodaTranslationStrategy>();
 builder.Services.AddScoped<ITranslationStrategy, ShakespeareTranslationStrategy>();
 builder.Services.AddScoped<ITranslationStrategyFactory, TranslationStrategyFactory>();
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Exception handler
 builder.Services.AddSingleton<GlobalExceptionHandler>();
 
 var app = builder.Build();
@@ -32,10 +37,16 @@ app.UseExceptionHandler(errorApp =>
         var feature = context.Features.Get<IExceptionHandlerFeature>();
         var exception = feature?.Error ?? new Exception("Unknown error");
 
-        var handler = context.RequestServices.GetService<GlobalExceptionHandler>() ?? new GlobalExceptionHandler();
+        var handler = context.RequestServices.GetService<GlobalExceptionHandler>() 
+            ?? new GlobalExceptionHandler();
         await handler.TryHandleAsync(context, exception, CancellationToken.None);
     });
 });
 
 app.MapControllers();
+
+Console.WriteLine("🚀 Pokedex API Starting...");
+Console.WriteLine("🔌 Listening on: http://0.0.0.0:5094");
+Console.WriteLine("📚 Swagger UI: http://localhost:5094/swagger");
+
 app.Run();
--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7974317adf7cf3c717cf3a67b7e00921f91e62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46d287c6a7f07fbc7c6064d735c68d8de61b65d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
